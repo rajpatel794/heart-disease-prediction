c90f4bbfee47d4757df6da86d18cb8ca2a2f131c
Suraj Patel <rajpatel794@gmail.com>
2025-02-21 21:29:48 +0530

    add javascript

---
+++ test.js
@@ -0,0 +1,6 @@
+function   greet(name){console.log("Hello, " + name +"!");}
+const add=(a,b)=>{return a+b;};
+
+if (true){console.log("This is a test");} 
+
+const numbers=[1,2,3,4,5];numbers.forEach(num=>{console.log(num)});
