e073840553226a768035cdff4d45668bb4064b8d
Suraj Patel <rajpatel794@gmail.com>
2025-02-21 21:07:56 +0530

    Update test.java

--- test.java
+++ test.java
@@ -3,7 +3,7 @@ package com.example;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Test {
+public class test {
   public static void main(String[] args) {
     List<String> names = new ArrayList<>();
     names.add("Alice");
