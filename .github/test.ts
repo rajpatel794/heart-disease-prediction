85d3ef583c054358742163af4ae3c949880733ac
Suraj Patel <rajpatel794@gmail.com>
2025-02-21 21:35:39 +0530

    Create test.ts

---
+++ .github/test.ts
@@ -0,0 +1,9 @@
+function   greet(name:string){console.log("Hello, " + name +"!");}
+const add=(a:number,b:number):number=>{return a+b;};
+
+interface Person{name:string;age:number;}
+const person:Person={name:"Alice",age:25};
+
+if (true){console.log("This is a test");} 
+
+const numbers:number[]=[1,2,3,4,5];numbers.forEach(num=>{console.log(num)});
